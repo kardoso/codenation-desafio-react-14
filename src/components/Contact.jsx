64563093f8885c3c6ec59963c1b67c2754be3f8a
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-14 20:48:18 -0300

    Exibe contatos

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from 'react'
+import dayjs from 'dayjs'
 
 class Contact extends React.Component {
   render() {
-    return (<div />);
+    const {
+      avatar,
+      name,
+      phone,
+      country,
+      admissionDate,
+      company,
+      department,
+    } = this.props.data
+    return (
+      <article className="contact">
+        <span className="contact__avatar">
+          <img src={avatar} alt="UserAvatar" />
+        </span>
+        <span className="contact__data" data-testid="contact-name">{name}</span>
+        <span className="contact__data" data-testid="contact-phone">{phone}</span>
+        <span className="contact__data" data-testid="contact-country">{country}</span>
+        <span className="contact__data" data-testid="contact-date">{dayjs(admissionDate).format("DD/MM/YYYY")}</span>
+        <span className="contact__data" data-testid="contact-company">{company}</span>
+        <span className="contact__data">{department}</span>
+      </article>
+    )
   }
 }
 
-export default Contact;
+export default Contact
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
+import Contact from './Contact'
+
 class Contacts extends React.Component {
+	state = {
+		contacts: [],
+	}
+
+	componentDidMount() {
+		fetch('https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts').then(
+			(response) => { return response.json() }
+		).then((data) => {
+			this.setState(() => ({ contacts: data }))
+		})
+	}
+
   render() {
     return (
       <div className="container">
@@ -14,6 +28,9 @@ class Contacts extends React.Component {
             <span className="contact__data">Empresa</span>
             <span className="contact__data">Departamento</span>
           </article>
+          {this.state.contacts.map((contact) => (
+						<Contact key={contact.id} data={contact} />
+          ))}
         </section>
       </div>
     )
